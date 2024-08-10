79c4e7ac81c50d93ed6ded54cb84243138be91f5
xiaoliangcheng <2637341276@qq.com>
2024-08-10 16:28:33 +0800

    提交父工程和子工程及api接口

---
+++ xuecheng-content-api/src/main/java/com/xlc/XuechengContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.xlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengContentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengContentApiApplication.class, args);
+    }
+
+}
